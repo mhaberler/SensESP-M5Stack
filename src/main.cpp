44b24bd8270a85418e1edef1b39f37106ba055d3
Michael Haberler <git@mah.priv.at>
2022-06-04 12:35:21 +0200

    SensESP examples for M5Stack Core2 and M5Stick-C

--- src/main.cpp
+++ src/main.cpp
@@ -6,118 +6,178 @@
 //
 // You can use this source file as a basis for your own projects.
 // Remove the parts that are not relevant to you, and add your own code
-// for external hardware libraries.
-
-#include "sensesp/sensors/analog_input.h"
-#include "sensesp/sensors/digital_input.h"
+// for eYternal hardware libraries.
 #include "sensesp/sensors/sensor.h"
+#include "sensesp/signalk/signalk_listener.h"
 #include "sensesp/signalk/signalk_output.h"
+#include "sensesp/signalk/signalk_value_listener.h"
 #include "sensesp/system/lambda_consumer.h"
+#include "sensesp/system/system_status_led.h"
 #include "sensesp_app_builder.h"
+
+#ifdef M5STICKC
+#include "M5StickCPlus.h"
+#endif
+
+#ifdef M5CORE2
+#include <M5Core2.h>
+#endif
+
+#ifdef AMMETER
+#include "M5_ADS1115.h"
+#endif
 
 using namespace sensesp;
 
 reactesp::ReactESP app;
+
+SKOutputFloat* accelX =
+    new SKOutputFloat("sensors.imu.accelX", "/sensors/imu/accelx",
+                      new SKMetadata("G", "acceleration in X direction"));
+
+SKOutputFloat* accelY =
+    new SKOutputFloat("sensors.imu.accelY", "/sensors/imu/accely",
+                      new SKMetadata("G", "acceleration in Y direction"));
+
+SKOutputFloat* accelZ =
+    new SKOutputFloat("sensors.imu.accelZ", "/sensors/imu/accelz",
+                      new SKMetadata("G", "acceleration in Z direction"));
+
+SKOutputFloat* yaw = new SKOutputFloat("sensors.ahrs.yaw", "/sensors/ahrs/yaw",
+                                       new SKMetadata("deg", "yaw angle"));
+
+SKOutputFloat* roll =
+    new SKOutputFloat("sensors.ahrs.roll", "/sensors/ahrs/roll",
+                      new SKMetadata("deg", "roll angle"));
+
+SKOutputFloat* pitch =
+    new SKOutputFloat("sensors.ahrs.pitch", "/sensors/ahrs/pitch",
+                      new SKMetadata("deg", "pitch angle"));
+
+#ifdef AMMETER
+SKOutputFloat* current =
+    new SKOutputFloat("sensors.ammeter.current", "/sensors/ammeter/current",
+                      new SKMetadata("A", "current"));
+#endif
+
+#ifdef AMMETER
+Ammeter ammeter;
+#endif
+
+#ifdef LED_BUILTIN
+SystemStatusLed* led;
+#endif
+
+const unsigned int sensorInterval = 500;
+
+void readSensors() {
+  float accX, accY, accZ;
+  float y, p, r;
+
+  M5.IMU.getAccelData(&accY, &accY, &accZ);
+  accelX->emit(accX);
+  accelY->emit(accY);
+  accelZ->emit(accZ);
+
+  M5.IMU.getAhrsData(&p, &r, &y);
+  yaw->emit(y);
+  pitch->emit(p);
+  roll->emit(r);
+
+#ifdef AMMETER
+  current->emit(ammeter.getCurrent());
+#endif
+
+#ifdef M5CORE2
+  M5.Lcd.setTextColor(GREEN, BLACK);
+  M5.Lcd.setCursor(0, 120);
+  M5.Lcd.printf("accY,   accY,  accZ");
+  M5.Lcd.setCursor(0, 140);
+  M5.Lcd.printf("%5.2f  %5.2f  %5.2f G", accY, accY, accZ);
+
+  M5.Lcd.setCursor(0, 160);
+  M5.Lcd.printf("pitch,  roll,  yaw");
+  M5.Lcd.setCursor(0, 180);
+  M5.Lcd.printf("%5.2f  %5.2f  %5.2f deg", pitch, roll, yaw);
+#endif
+}
 
 // The setup function performs one-time application initialization.
 void setup() {
 #ifndef SERIAL_DEBUG_DISABLED
   SetupSerialDebug(115200);
 #endif
+  M5.begin();
+
+#ifdef LED_BUILTIN
+  led = new SystemStatusLed(LED_BUILTIN);
+#endif
+
+  M5.IMU.Init();
+
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(2);
+
+#ifdef AMMETER
+  ammeter.setMode(SINGLESHOT);
+  ammeter.setRate(RATE_8);
+  ammeter.setGain(PAG_512);
+#endif
 
   // Construct the global SensESPApp() object
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
                     // Set a custom hostname for the app.
-                    ->set_hostname("my-sensesp-project")
+                    ->set_hostname(HOST)
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
-                    //->set_wifi("My WiFi SSID", "my_wifi_password")
+                    //->set_wifi("imu-demo", "somepassword")
                     //->set_sk_server("192.168.10.3", 80)
+                    ->enable_system_info_sensors()
+                    ->enable_ota("thisisfine")
+#ifdef LED_BUILTIN
+                    ->set_system_status_led(led)
+#endif
                     ->get_app();
 
-  // GPIO number to use for the analog input
-  const uint8_t kAnalogInputPin = 36;
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 3.3;
+  const char* sk_altitude = "bmp390.altitude";
+  const int listen_delay = 1000;
+  auto* altitude = new FloatSKListener(sk_altitude, listen_delay);
+  altitude->connect_to(new LambdaConsumer<float>([](float a) {
+    M5.Lcd.setTextColor(YELLOW, BLACK);
 
-  // Create a new Analog Input Sensor that reads an analog input pin
-  // periodically.
-  auto* analog_input = new AnalogInput(
-      kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.printf(" alt: %.3f   ", a);
+  }));
 
-  // Add an observer that prints out the current value of the analog input
-  // every time it changes.
-  analog_input->attach([analog_input]() {
-    debugD("Analog input value: %f", analog_input->get());
-  });
+  const char* sk_vspeed = "bmp390.vspeed";
+  auto* vspeed = new FloatSKListener(sk_vspeed, listen_delay);
+  vspeed->connect_to(new LambdaConsumer<float>([](float vs) {
+    M5.Lcd.setTextColor(YELLOW, BLACK);
 
-  // Set GPIO pin 15 to output and toggle it every 650 ms
+    M5.Lcd.setCursor(0, 40);
+    M5.Lcd.printf(" vspeed: %3.3f   ", vs);
+  }));
 
-  const uint8_t kDigitalOutputPin = 15;
-  const unsigned int kDigitalOutputInterval = 650;
-  pinMode(kDigitalOutputPin, OUTPUT);
-  app.onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
-    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
-  });
+  const char* sk_gps_altitude = "navigation.gnss.antennaAltitude";
+  auto* gps_altitude = new FloatSKListener(sk_gps_altitude, listen_delay);
+  gps_altitude->connect_to(new LambdaConsumer<float>([](float gps_alt) {
+    M5.Lcd.setTextColor(YELLOW, BLACK);
 
-  // Read GPIO 14 every time it changes
+    M5.Lcd.setCursor(0, 60);
+    M5.Lcd.printf(" GPS alt: %.3f   ", gps_alt);
+  }));
 
-  const uint8_t kDigitalInput1Pin = 14;
-  auto* digital_input1 =
-      new DigitalInputChange(kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
+  const char* sk_gpsfix = "navigation.gnss.methodQuality";
+  auto* gpsfix = new StringSKListener(sk_gpsfix, listen_delay);
+  gpsfix->connect_to(new LambdaConsumer<String>([](String fix) {
+    M5.Lcd.setTextColor(YELLOW, BLACK);
+    M5.Lcd.setCursor(0, 80);
+    M5.Lcd.printf(" gpsfix: %s   ", fix);
+  }));
 
-  // Connect the digital input to a lambda consumer that prints out the
-  // value every time it changes.
-
-  // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
-  // see if the value changes!
-
-  digital_input1->connect_to(new LambdaConsumer<bool>(
-      [](bool input) { debugD("Digital input value changed: %d", input); }));
-
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
-  const uint8_t kDigitalInput2Pin = 13;
-  const unsigned int kDigitalInput2Interval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
-
-  auto* digital_input2 = new RepeatSensor<bool>(
-      kDigitalInput2Interval,
-      [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
-
-  // Connect the analog input to Signal K output. This will publish the
-  // analog input value to the Signal K server every time it changes.
-  analog_input->connect_to(new SKOutputFloat(
-      "sensors.analog_input.voltage",         // Signal K path
-      "/sensors/analog_input/voltage",        // configuration path, used in the
-                                              // web UI and for storing the
-                                              // configuration
-      new SKMetadata("V",                     // Define output units
-                     "Analog input voltage")  // Value description
-      ));
-
-  // Connect digital input 2 to Signal K output.
-  digital_input2->connect_to(new SKOutputBool(
-      "sensors.digital_input2.value",          // Signal K path
-      "/sensors/digital_input2/value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Digital input 2 value")  // Value description
-      ));
+  app.onRepeat(sensorInterval, []() { readSensors(); });
 
   // Start networking, SK server connections and other SensESP internals
   sensesp_app->start();
